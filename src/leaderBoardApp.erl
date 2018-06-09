8cf8b4c4b36fe40edf04719c53ce7e2de71053ca
JuliaZur <julia17044@gmail.com>
2018-06-09 21:14:58 +0200

    Added starter board

--- src/leaderBoardApp.erl
+++ src/leaderBoardApp.erl
@@ -17,24 +17,27 @@ start ()->
   Wx = wx:new(),
   Frame = wxFrame:new(Wx, -1, "Rebus Game", [{size, {500, 500}}]),
   Panel = wxPanel:new(Frame),
-  wxFrame:connect(Panel, paint),
-  wxPanel:connect(Panel, paint, [callback]),
-  wxPanel:connect(Panel, left_up, []),
-  wxPanel:connect(Panel, right_down, []),
-  wxPanel:connect(Panel, middle_down, []),
+  wxFrame:setMinSize(Frame,{500,500}),
+
+  Sizer = wxBoxSizer:new(?wxVERTICAL),
+
   wxPanel:setBackgroundColour(Panel,?wxWHITE),
-  Button1 = wxButton:new(Panel,1,[{pos,{100,250}},{label,"PLAY 1 TURN"}]),
+
+  Button1 = wxButton:new(Panel,1,[{label,"PLAY 1 TURN"},{size,{100,20}}]),
+  Button2 = wxButton:new(Panel,2,[{label,"PLAY 5 TURNS"},{size,{100,20}}]),
+  Button3 = wxButton:new(Panel,3,[{label,"PLAY 10 TURNS"},{size,{100,20}}]),
+  Texts = [wxStaticText:new(Panel,0,"",[]),wxStaticText:new(Panel,11,"Welcome to Rebus Game",[{style,?wxALIGN_CENTER},{size,{50,50}}]),
+    wxStaticText:new(Panel,12,"Choose numer of turns to play",[{style,?wxALIGN_CENTER},{size,{50,50}}])],
+  [wxSizer:add(Sizer,Text,[{flag,?wxEXPAND bor ?wxALIGN_CENTER},{proportion,1},{border,10}]) || Text <- Texts],
+  wxSizer:add(Sizer,Button1,[{flag,?wxEXPAND bor ?wxALL},{proportion,1},{flag,?wxALIGN_CENTER},{border,10}]),
+  wxSizer:add(Sizer,Button2,[{flag,?wxEXPAND bor ?wxALL},{proportion,1},{flag,?wxALIGN_CENTER},{border,10}]),
+  wxSizer:add(Sizer,Button3,[{flag,?wxEXPAND bor ?wxALL},{proportion,1},{flag,?wxALIGN_CENTER},{border,10}]),
+
+  wxPanel:setSizer(Panel,Sizer),
+  wxSizer:fit(Sizer,Panel),
 
   OnPaint = fun(_Evt, _Obj) ->
     Paint = wxPaintDC:new(Panel),
-    Font = wxFont:new(30, ?wxFONTFAMILY_MODERN, ?wxFONTSTYLE_NORMAL, ?wxFONTWEIGHT_BOLD),
-    wxDC:setFont(Paint,Font),
-    wxDC:drawText(Paint, "Welcome to Rebus game!", {60, 100}),
-    wxFont:destroy(Font),
-    Font2 = wxFont:new(14,?wxFONTFAMILY_MODERN, ?wxFONTSTYLE_NORMAL, ?wxFONTWEIGHT_LIGHT),
-    wxDC:setFont(Paint,Font2),
-    wxDC:drawText(Paint, "Please enter your username and then click START", {60,150}),
-    wxFont:destroy(Font2),
     Pen = wxPen:new(),
     wxPen:setWidth(Pen,5),
     wxPen:setColour(Pen,102,192,255),
@@ -55,9 +58,10 @@ start ()->
     wxDC:drawCircle(Paint,{260,190},5),
     wxPaintDC:destroy(Paint),
     wxPen:destroy(Pen3)
-    end,
+            end,
 
   wxFrame:connect(Panel, paint, [{callback, OnPaint}]),
   wxFrame:connect(Panel, close_window),
   wxFrame:center(Frame),
+  wxFrame:fit(Frame),
   wxFrame:show(Frame).
