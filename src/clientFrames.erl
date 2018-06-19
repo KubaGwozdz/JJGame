5072d605e05e7d83204e1b7ac450c878edf5c4f7
Jakub Gwóźdź <jakub.gwozdz@icloud.com>
2018-06-19 11:34:08 +0200

    controller update

--- src/clientFrames.erl
+++ src/clientFrames.erl
@@ -274,9 +274,8 @@ your_points_refresh({Frame, Panel}, Rebus,  Name, Points, Sizer) ->
       io:format("~p Closing window ~n",[self()]),
       wxWindow:destroy(Frame),
       ok;
-    {give_answer, R} ->
-      clientFrames:show_give_answer_frame({Frame, Panel}, R, Name)
-
+    {choose, Reb} ->
+      clientFrames:show_choose_answer_frame({Frame, Panel},Reb, Name)
     after
       10000 -> wxStaticText:setLabel(Points, [integer_to_list(boardServer:get_clients_points(self()))]) %show_your_points_frame({Frame, Panel}, Rebus, Name)
   end.
